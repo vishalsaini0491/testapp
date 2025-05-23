ea7d089629699e1d918a6f07fb5bd87b460d5250
vishalsaini0491 <vishalsaini.personal0601@gmail.com>
2025-05-23 15:51:48 +0530

    Initial commit

---
+++ android/app/src/main/java/com/testapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.testapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>|| Jai Shree Ram ||</h1>
+    </>
+  )
+}
+
+export default App
